ad7de5aad2a692f15b561b8b390a13d5e608c278
Luke LeFevers <luke.lefevers@gmail.com>
2026-05-29 12:13:50 -0700

    Initial commit

---
+++ gbinfo.py
@@ -0,0 +1,71 @@
+from sys import argv
+
+def header_checksum(file):
+    checksum = 0
+    for address in range(0x134, 0x14D):
+        checksum = checksum - file[address] - 1
+    checksum &= 0b11111111
+    return checksum
+
+def global_checksum(file):
+    checksum = 0
+    for address, value in enumerate(file):
+        if not address in (0x14E, 0x14F):
+            checksum += value
+            checksum &= 0b1111111111111111
+    return checksum
+
+mappers = {0x00: "ROM ONLY", 0x01: "MBC1", 0x02: "MBC1+RAM", 0x03: "MBC1+RAM+BATTERY", 0x05: "MBC2", 0x06: "MBC2+BATTERY", 0x08: "ROM+RAM", 0x09: "ROM+RAM+BATTERY", 0x0B: "MMM01", 0x0C: "MMM01+RAM", 0x0D: "MMM01+RAM+BATTERY", 0x0F: "MBC3+TIMER+BATTERY", 0x10: "MBC3+TIMER+RAM+BATTERY", 0x11: "MBC3", 0x12: "MBC3+RAM", 0x13: "MBC3+RAM+BATTERY", 0x19: "MBC5", 0x1A: "MBC5+RAM", 0x1B: "MBC5+RAM+BATTERY", 0x1C: "MBC5+RUMBLE", 0x1D: "MBC5+RUMBLE+RAM", 0x1E: "MBC5+RUMBLE+RAM+BATTERY", 0x20: "MBC6", 0x22: "MBC7+SENSOR+RUMBLE+RAM+BATTERY", 0xFC: "POCKET CAMERA", 0xFD: "BANDAI TAMA5", 0xFE: "HuC3", 0xFF: "HuC1+RAM+BATTERY"}
+cgb_compats = {0x80: "CGB enhanced, DMG compatible", 0xC0: "CGB only"}
+sgb_compat = {0x03: "Compatible"}
+ram_sizes = {0x00: "None", 0x01: "2 KiB", 0x02: "8 KiB", 0x03: "32 KiB", 0x04: "128 KiB", 0x05: "64 KiB"}
+region = {0x00: "Japan", 0x01: "Overseas"}
+
+def main():
+    if len(argv) < 2:
+        print("Usage: gbinfo %CARTRIDGE_NAME%")
+        return 0
+
+    try:
+        file = open(argv[1], "rb")
+    except OSError as error:
+        print(error)
+        return 0
+
+    file_contents = file.read()
+    print(f"Cartridge info for {argv[1]}:\n")
+
+    if file_contents[0x143] in cgb_compats:
+        print(f"    Game title: {file_contents[0x134:0x143]}")
+        print(f"    CGB compatibility: {file_contents[0x143]} ({cgb_compats.get(file_contents[0x143], "DMG only")})")
+    else:
+        print(f"    Game title: {file_contents[0x134:0x144]}")
+
+    if file_contents[0x14B] == 0x33:
+        print(f"    Publisher: {file_contents[0x144:0x146]}")
+    else:
+        print(f"    Publisher: {file_contents[0x14B]}")
+
+    print(f"    SGB compatibility: {file_contents[0x146]} ({sgb_compat.get(file_contents[0x146], "Incompatible")})")
+    print(f"    Cartridge hardware: {file_contents[0x147]} ({mappers.get(file_contents[0x147], "Invalid value")})")
+    print(f"    ROM size: {file_contents[0x148]} ({32 * (1 << file_contents[0x148])} KiB)")
+    print(f"    RAM size: {file_contents[0x149]} ({ram_sizes.get(file_contents[0x149], "Invalid value")})")
+    print(f"    Region: {file_contents[0x14A]} ({region.get(file_contents[0x14A], "Invalid value")})")
+    print(f"    Revision: {file_contents[0x14C]}")
+
+    print(f"    Header checksum...")
+    checksum = header_checksum(file_contents)
+    if checksum == file_contents[0x14D]:
+        print(f"Checks out. ({checksum})")
+    else:
+        print(f"Doesn't check out. (stored: {file_contents[0x14D]}, calculated: {checksum})")
+
+    print("    Global checksum...")
+    checksum = global_checksum(file_contents)
+    if checksum == (file_contents[0x14E] << 8) + file_contents[0x14F]:
+        print(f"Checks out. ({checksum})")
+    else:
+        print(f"Doesn't check out. (stored: {(file_contents[0x14E] << 8) + file_contents[0x14F]}, calculated: {checksum})")
+
+if __name__ == "__main__":
+    main()
---
+++ nesinfo.py
@@ -0,0 +1,45 @@
+from sys import argv
+
+if len(argv) < 2:
+    print("Usage: nesinfo %CARTRIDGE_NAME%")
+    exit(2)
+
+nametable_arrs = {0b0: "Vertical", 0b1: "Horizontal"}
+present = {0b1: "Present"}
+yes = {0b1: "Yes"}
+nes2 = {0b10: "Yes"}
+
+try:
+    file = open(argv[1], "rb")
+except OSError as error:
+    print(error)
+    exit(2)
+
+file_contents = file.read()
+print(f"Cartridge info for {argv[1]}:\n")
+
+if file_contents[0x0:0x4] == b"NES\x1A":
+    print(f"    Header matches. ({file_contents[0x0:0x4]})")
+else:
+    print(f"    Header doesn't match. (stored: {file_contents[0x0:0x4]}, expected: b'NES\\x1a')")
+
+nes2_v = (file_contents[0x7] & 0b00001100) >> 2
+if nes2_v == 0b10:
+    print(f"    PRG-ROM size: {(file_contents[0x9] & 0b00001111) << 8 + file_contents[0x4]} ({(file_contents[0x9] & 0b00001111) << 8 + file_contents[0x4] * 16} KiB)")
+    print(f"    CHR-ROM size: {(file_contents[0x9] & 0b11110000) << 4 + file_contents[0x5]} ({(file_contents[0x9] & 0b11110000) << 4 + file_contents[0x5] * 8} KiB)")
+else:
+    print(f"    PRG-ROM size: {file_contents[0x4]} ({file_contents[0x4] * 16} KiB)")
+    print(f"    CHR-ROM size: {file_contents[0x5]} ({file_contents[0x5] * 8} KiB)")
+print(f"    Nametable arrangement: {file_contents[0x6] & 0b00000001} ({nametable_arrs.get(file_contents[0x6] & 0b00000001)}), alt: {(file_contents[0x6] & 0b00001000) >> 3} ({yes.get((file_contents[0x6] & 0b00001000) >> 3, "No")})")
+print(f"    Flash memory: {(file_contents[0x6] & 0b00000010) >> 1} ({present.get((file_contents[0x6] & 0b00000010) >> 1, "Not present")})")
+print(f"    Trainer: {(file_contents[0x6] & 0b00000100) >> 2} ({present.get((file_contents[0x6] & 0b00000100) >> 2, "Not present")})")
+
+if nes2_v == 0b10:
+    print(f"    Mapper ID: {(file_contents[0x8] & 0b00001111 << 8) + (file_contents[0x7] & 0b11110000) + (file_contents[0x6] & 0b11110000) >> 4}")
+else:
+    print(f"    Mapper ID: {(file_contents[0x7] & 0b11110000) + (file_contents[0x6] & 0b11110000) >> 4}")
+print(f"    VS. System: {file_contents[0x7] & 0b00000001} ({yes.get(file_contents[0x7] & 0b00000001, "No")})")
+print(f"    PlayChoice-10: {(file_contents[0x7] & 0b00000010) >> 1} ({yes.get((file_contents[0x7] & 0b00000010) >> 1, "No")})")
+print(f"    NES 2.0: {nes2_v} ({nes2.get(nes2_v, "No")})")
+if nes2_v == 0b10:
+    print(f"Submapper: {(file_contents[0x8] & 0b11110000) >> 4}")
